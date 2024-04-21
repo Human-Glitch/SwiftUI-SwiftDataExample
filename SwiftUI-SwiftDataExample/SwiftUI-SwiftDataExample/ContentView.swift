3aa7cda0e4cc0943ea92fe1376e3a8d5bd81302b
Kody Buss <kody.buss@gmail.com>
2024-04-21 18:22:23 -0500

    init

---
+++ SwiftUI-SwiftDataExample/SwiftUI-SwiftDataExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-SwiftDataExample
+//
+//  Created by Kody Buss on 4/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-SwiftDataExample/SwiftUI-SwiftDataExample/SwiftUI_SwiftDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SwiftDataExampleApp.swift
+//  SwiftUI-SwiftDataExample
+//
+//  Created by Kody Buss on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SwiftDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
