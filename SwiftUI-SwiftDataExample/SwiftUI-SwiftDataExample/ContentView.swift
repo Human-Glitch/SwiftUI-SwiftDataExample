2148dc1ed66cfcfae2786fa6f18f986210084a3e
Kody Buss <kody.buss@gmail.com>
2024-04-21 19:33:05 -0500

    Add expense cell view

--- SwiftUI-SwiftDataExample/SwiftUI-SwiftDataExample/ContentView.swift
+++ SwiftUI-SwiftDataExample/SwiftUI-SwiftDataExample/ContentView.swift
@@ -22,3 +22,19 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+struct ExpenseCell: View {
+	let expense: Expense
+	
+	var body: some View {
+		HStack {
+			Text(expense.date, format: .dateTime.month(.abbreviated).day())
+				.frame(width: 70, alignment: .leading)
+			Text(expense.name)
+			
+			Spacer()
+			
+			Text(expense.value, format: .currency(code: "USD"))
+		}
+	}
+}
