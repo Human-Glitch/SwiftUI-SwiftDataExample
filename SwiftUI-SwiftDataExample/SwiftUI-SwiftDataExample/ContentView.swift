1494562f301f10ea4706f246c41064f4acb29414
Kody Buss <kody.buss@gmail.com>
2024-04-21 19:33:40 -0500

    Update expense sheet

--- SwiftUI-SwiftDataExample/SwiftUI-SwiftDataExample/ContentView.swift
+++ SwiftUI-SwiftDataExample/SwiftUI-SwiftDataExample/ContentView.swift
@@ -73,3 +73,30 @@ struct AddExpenseSheet: View {
 	}
 }
 
+struct UpdateExpenseSheet: View {
+	@Environment(\.modelContext) var context
+	@Environment(\.dismiss) private var dismiss
+	
+	@Bindable var expense: Expense
+	
+	var body: some View {
+		NavigationStack {
+			Form {
+				TextField("Expense Name", text: $expense.name)
+				DatePicker("Date", selection: $expense.date, displayedComponents: .date)
+				TextField("Value", value: $expense.value, format: .currency(code: "USD"))
+					.keyboardType(.decimalPad)
+			}
+			.navigationTitle("Update Expense")
+			.navigationBarTitleDisplayMode(.large)
+			.toolbar {
+				ToolbarItemGroup(placement: .topBarTrailing) {
+					Button("Done") {
+						dismiss()
+					}
+				}
+				
+			}
+		}
+	}
+}
