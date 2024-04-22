38a9fde4e2f015f9987375af3c59acba166a9e86
Kody Buss <kody.buss@gmail.com>
2024-04-21 19:35:14 -0500

    Set up content view to add/delete/update expenses

--- SwiftUI-SwiftDataExample/SwiftUI-SwiftDataExample/ContentView.swift
+++ SwiftUI-SwiftDataExample/SwiftUI-SwiftDataExample/ContentView.swift
@@ -16,14 +16,49 @@ struct ContentView: View {
 	var expenses: [Expense]
 	
 	@State private var expenseToEdit: Expense?
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		NavigationStack{
+			List {
+				ForEach(expenses) { expense in
+					ExpenseCell(expense: expense)
+						.onTapGesture {
+							expenseToEdit = expense
+						}
+				}
+				.onDelete { indexSet in
+					for index in indexSet {
+						context.delete(expenses[index])
+					}
+				}
+			}
+			.navigationTitle("Expenses")
+			.navigationBarTitleDisplayMode(.large)
+			.sheet(isPresented: $isShowingItemSheet) { AddExpenseSheet() }
+			.sheet(item: $expenseToEdit) { expense in
+				UpdateExpenseSheet(expense: expense)
+			}
+			.toolbar {
+				if !expenses.isEmpty {
+					Button("Add Expense", systemImage: "plus") {
+						isShowingItemSheet = true
+					}
+				}
+			}
+			.overlay {
+				if expenses.isEmpty {
+					ContentUnavailableView(label: {
+						Label("No Expenses", systemImage: "list.bullet.rectangle.portrait")
+					}, description: {
+						Text("Start adding expenses to see your list")
+					}, actions: {
+						Button("Add Expenses") { isShowingItemSheet = true }
+					})
+					.offset(y: -60)
+				}
+			}
+			
+		}
     }
 }
 
